7d48c836170de7a89e942c4f36988c0626814434
roman <rgnevashev@gmail.com>
2016-02-04 08:35:58 +0300

    update

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'rgnevashev:angular-bootstrap',
-  version: '1.0.3',
+  version: '1.1.2',
   summary: 'Native AngularJS (Angular) directives for Bootstrap.',
   git: 'https://github.com/angular-ui/bootstrap',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  //api.versionsFrom('1.2.1');
   api.addFiles([
     "./ui-bootstrap-tpls.js"
   ], 'client');
